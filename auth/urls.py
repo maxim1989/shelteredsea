7fc31280c2110d94a3ae9823d5da7e4a0074afcf
Maxim <dizard386@yandex.ru>
2016-07-17 00:29:11 +0300

    #6

--- auth/urls.py
+++ auth/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.logged, name='logged'),
     url(r'^login_fail/$', views.logged_fail, name='logged_fail'),
-    url(r'^logout/$', views.logout, name='user_logout'),
+    # url(r'^logout/$', views.logout, name='user_logout'),
 ]
