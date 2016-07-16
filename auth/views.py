6320f9e69a0f3a475343e7cc7a654a480d043147
Maxim <dizard386@yandex.ru>
2016-07-16 22:22:45 +0300

    попытка залогиниться #6

---
+++ auth/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import logout
+from django.shortcuts import render
+
+# Create your views here.
+def logged(request):
+    return render(request, 'core/index.html', {'page_status': 'login'})
+
+
+def logged_fail(request):
+    return render(request, 'core/index.html', {'page_status': 'fail'})
+
+
+def logout(request):
+    logout(request)
+    return render(request, 'core/index.html', {'page_status': 'logout'})
--- core/views.py
+++ core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'core/index.html')
+    return render(request, 'core/index.html', {'page_status': 'start'})
