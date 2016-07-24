8b49e78a84a20ab2c56807417e5efca2071e9fbb
Maxim <dizard386@yandex.ru>
2016-07-24 13:13:46 +0300

    GetCurrentUserInfo: правки #9

--- auth/serializers.py
+++ auth/serializers.py
@@ -4,9 +4,8 @@ from rest_framework import serializers
 
 
 class AuthenticatedUserSerializer(serializers.ModelSerializer):
-    provider = serializers.CharField(source='usersocialauth.provider')
 
     class Meta:
         model = User
         fields = ('id', 'last_login', 'is_superuser', 'username', 'first_name', 'last_name', 'email',
-                  'is_staff', 'is_active', 'date_joined', 'provider')
+                  'is_staff', 'is_active', 'date_joined')
--- auth/views.py
+++ auth/views.py
@@ -1,13 +1,8 @@
 from django.contrib import auth
-# from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.shortcuts import HttpResponseRedirect, render
-# from django.views.decorators.csrf import ensure_csrf_cookie
-# from django.views.decorators.csrf import  csrf_exempt
-
-# from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 
 from auth.serializers import AuthenticatedUserSerializer
@@ -27,7 +22,7 @@ def logged_fail(request):
 
 def logout(request):
     auth.logout(request)
-    return JSONResponse(data=dict(), is_autorized=False)
+    return JSONResponse(dict(), is_autorized=False)
 
 
 class JSONResponse(HttpResponse):
@@ -35,14 +30,12 @@ class JSONResponse(HttpResponse):
     An HttpResponse that renders its content into JSON.
     """
     def __init__(self, data, **kwargs):
-        data['is_autorized'] = kwargs.get('is_autorized', True)
+        data['is_autorized'] = kwargs.pop('is_autorized')
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-# @ensure_csrf_cookie
-# @csrf_exempt
 def authenticated_user(request):
     """
     Получить сведения об авторизированном пользователе
@@ -53,7 +46,7 @@ def authenticated_user(request):
         if request.user.is_authenticated():
             user_id = request.user.id
             user = User.objects.get(pk=user_id)
-            serializer = AuthenticatedUserSerializer(data=user)
-            return JSONResponse(data=serializer.data)
+            serializer = AuthenticatedUserSerializer(user)
+            return JSONResponse(serializer.data, is_autorized=True)
         else:
-            return JSONResponse(data=dict(), is_autorized=False)
+            return JSONResponse(dict(), is_autorized=False)
