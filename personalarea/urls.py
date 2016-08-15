a7b3394077dfb147b44294b7a2f54aaaba127e70
Maxim <dizard386@yandex.ru>
2016-08-15 22:04:42 +0300

    HallAndPersonal: вывод всех пользователей и одного для редактирования #16 #17

--- personalarea/urls.py
+++ personalarea/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     url(r'^$', views.FriendsList.as_view()),
     url(r'^(?P<uid_for_client>[0-9]+)$', views.FindUser.as_view()),
     url(r'^(?P<uid_for_client>[0-9]+)/accept$', views.Accept.as_view()),
-    url(r'^(?P<uid_for_client>[0-9]+)/delete', views.Delete.as_view()),
-    url(r'^(?P<uid_for_client>[0-9]+)/ignore', views.Ignore.as_view()),
+    url(r'^(?P<uid_for_client>[0-9]+)/delete$', views.Delete.as_view()),
+    url(r'^(?P<uid_for_client>[0-9]+)/ignore$', views.Ignore.as_view()),
+    url(r'^statistic$', views.HallOfFame.as_view()),
+    url(r'^personal$', views.PersonalData.as_view()),
 ]
