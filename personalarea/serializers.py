b20b66e55eae5f33289b79c1aa36e57278fb261a
Maxim <dizard386@yandex.ru>
2016-08-28 13:31:49 +0300

    SerializersCurrentModel: привёл serializers к одной модели #11

--- personalarea/serializers.py
+++ personalarea/serializers.py
@@ -2,26 +2,13 @@ from rest_framework import serializers
 
 from .models import Friends
 
+from loginsys.serializers import AuthenticatedUserSerializer
+
 
 class FriendsSerializer(serializers.ModelSerializer):
-    uid_for_client = serializers.SerializerMethodField()
-    statistic_name = serializers.SerializerMethodField()
-    dispute_name = serializers.SerializerMethodField()
-    username = serializers.SerializerMethodField()
-
-    def get_uid_for_client(self, obj):
-        return obj.user.uid_for_client.name
-
-    def get_statistic_name(self, obj):
-        return obj.user.statistic_name.name
-
-    def get_dispute_name(self, obj):
-        return obj.user.dispute_name.name
-
-    def get_username(self, obj):
-        return obj.user.username
+    user = AuthenticatedUserSerializer()
+    user_friend = AuthenticatedUserSerializer()
 
     class Meta:
         model = Friends
-        fields = ('uid_for_client', 'user_friend', 'statistic_name', 'user', 'is_friend', 'is_ignore', 'username',
-                  'dispute_name')
+        fields = ('user_friend', 'user', 'is_friend', 'is_ignore')
