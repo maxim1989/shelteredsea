2037f4d9419ae622537ba60db414ae3b068720d3
Maxim <dizard386@yandex.ru>
2016-11-05 19:43:45 +0300

    MakeBackgrountTask: Настроить фоновую задачу #31

--- core/tasks.py
+++ core/tasks.py
@@ -1,8 +1,15 @@
 from __future__ import absolute_import
 
+import os
+
+import requests
 from celery import shared_task
 
 
 @shared_task
 def test():
     print("Test********************")
+    url = 'http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1?key={0}'.format(os.environ.get('SOCIAL_AUTH_STEAM_API_KEY'))
+    response = requests.get(url)
+    print(response.text)
+    print(type(response.text))
