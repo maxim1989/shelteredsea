5078f5c5b8eef85522990be6b4e47ddd4692407d
Maxim <dizard386@yandex.ru>
2016-11-20 14:05:37 +0300

    CloseDealInBackground: fix 2 #32

--- core/tasks.py
+++ core/tasks.py
@@ -12,25 +12,22 @@ from disputes.models import Deals, OrderForDeal
 
 @shared_task
 def test():
-    print("Test********************")
     deals = Deals.objects.filter(is_active=True)
-    print(deals)
-
     for deal in deals:
-        print('---------------------------------')
-        deal_time_stamp = (deal.created - datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)).total_seconds()
-        print(deal_time_stamp)
-        orders = OrderForDeal.objects.filter(deal=deal)
-        deal.is_active = False
-        deal.save()
-        for order in orders:
-            print('================================')
-            # steam_id = UserSocialAuth.objects.get(user_id=order.user.id)
-            # steam_id_32 = int(steam_id.uid) & ((1 << 32) - 1)
-            # url = 'http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1?key={0}&date_min={1}&account_id={2}'.format(
-            #     os.environ.get('SOCIAL_AUTH_STEAM_API_KEY'), deal_time_stamp, steam_id_32
-            # )
-            # response = requests.get(url)
-            # print(response.text)
-            order.is_active = False
-            order.save()
+        # deal_time_stamp = (deal.created - datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)).total_seconds()
+        # print(deal_time_stamp)
+        interval = datetime.datetime.now(datetime.timezone.utc) - deal.created
+        if interval.total_seconds() > 300:
+            orders = OrderForDeal.objects.filter(deal=deal)
+            deal.is_active = False
+            deal.save()
+            for order in orders:
+                # steam_id = UserSocialAuth.objects.get(user_id=order.user.id)
+                # steam_id_32 = int(steam_id.uid) & ((1 << 32) - 1)
+                # url = 'http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1?key={0}&date_min={1}&account_id={2}'.format(
+                #     os.environ.get('SOCIAL_AUTH_STEAM_API_KEY'), deal_time_stamp, steam_id_32
+                # )
+                # response = requests.get(url)
+                # print(response.text)
+                order.is_active = False
+                order.save()
